45e6ecda22e78c633ca98abf07f09af4c761bcf0
bagher2g <bagher2g@gmail.com>
2020-12-18 19:27:43 +0330

    init

---
+++ Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('kajpanel::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('kajpanel.name') !!}
+    </p>
+@endsection
